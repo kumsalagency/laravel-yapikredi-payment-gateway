aa0058ca1c4c1a3cf1cd773f213aaa70df4679ad
Emre <bilgi@emrecaliskan.com.tr>
2020-10-15 15:57:47 +0300

    data fix

--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -3,7 +3,7 @@
 @section('body')
     <form method="post" action="{{ $formData['gateway'] ?? '' }}" id="threeDForm">
         @foreach($formData['inputs'] ?? [] as $key => $value)
-            <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+            <input type="hidden" name="{!! $key !!}" value="{!! $value !!}">
         @endforeach
         <button class="theme-btn btn-style-one btn-reverse" type="submit"><span>@lang('payment::buttons.redirect')</span></button>
     </form>
